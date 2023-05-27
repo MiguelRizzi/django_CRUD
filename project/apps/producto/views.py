fd3834e5366ceb66aaefd6136f9f8f03fb1fcd2b
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-05-27 05:07:59 -0300

    Se creo formulario para crear categoria de productos

--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -9,3 +10,13 @@ def producto_categoria_list(request):
     categorias = models.ProductoCategoria.objects.all()
     context = {"categorias": categorias}
     return render(request, "producto/producto_categoria_list.html", context)
+
+def producto_categoria_create(request):
+    if request.method == "POST":
+        form = forms.ProductoCategoriaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("producto:producto_categoria_list")
+    else:
+        form = forms.ProductoCategoriaForm()
+    return render(request, "producto/producto_categoria_create.html", {"form": form})
