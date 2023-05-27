0fa52711aa41224d0e90fd0da63d2ebd13e40dea
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-05-27 03:38:01 -0300

    Se agregan models en productos.

--- project/apps/producto/models.py
+++ project/apps/producto/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class ProductoCategoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Categoria de producto'
+        verbose_name_plural = "Categorias de productos"
+
+    def __str__(self):
+        return self.nombre
