9fe40e864cfbaa2975b28b564702e5f59d7888c6
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-05-27 23:19:00 -0300

    Views basadas en clases

--- project/apps/producto/views.py
+++ project/apps/producto/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render, redirect
-from . import models
-from . import forms
+from . import models, forms
+from django.urls import reverse_lazy
+
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 # Create your views here.
 
 def index(request):
     return render(request, "producto/index.html")
 
+# Vistas basadas en clases
+class ProductoCategoriaList(ListView):
+    model = models.ProductoCategoria
+    template_name = "producto/producto_categoria_list.html"
+    context_object_name= "categorias"
+
+class ProductoCategoriaCreate(CreateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
+    template_name = "producto/producto_categoria_create.html"
+    success_url = reverse_lazy("producto:producto_categoria_list")
+    
+class ProductoCategoriaUpdate(UpdateView):
+    model = models.ProductoCategoria
+    form_class = forms.ProductoCategoriaForm
+    template_name = "producto/producto_categoria_update.html"
+    success_url = reverse_lazy("producto:producto_categoria_list")
+
+class ProductoCategoriaDelete(DeleteView):
+    model = models.ProductoCategoria
+    template_name = "producto/producto_categoria_delete.html"
+    success_url = reverse_lazy("producto:producto_categoria_list")
+
+
+# Vistas basadas en funciones
+"""
 def producto_categoria_list(request):
     categorias = models.ProductoCategoria.objects.all()
     context = {"categorias": categorias}
@@ -38,3 +67,4 @@ def producto_categoria_delete(request, id):
         categoria.delete()
         return redirect("producto:producto_categoria_list")
     return render(request, "producto/producto_categoria_delete.html", {"categoria": categoria})
+"""
