f306c829cadddf76923c07dde2936766a7f77b7a
Miguel Angel Rizzi <miguel-rizzi@hotmail.com.ar>
2023-05-30 23:50:17 -0300

    Se agrego login de usuarios

--- project/apps/home/urls.py
+++ project/apps/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, login_request
 
 urlpatterns = [
     path('', index, name="index"),
+    path("login/", login_request, name="login"),
 ]
--- project/apps/home/views.py
+++ project/apps/home/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 
+# Login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+
 # Create your views here.
 
 def index(request):
     return render(request, "home/index.html")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST, data=request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, "home/index.html", {"mensaje": f"Bienvenido {usuario}!"})
+    else:
+        form = AuthenticationForm()
+    return render(request, "home/login.html", {"form": form})
